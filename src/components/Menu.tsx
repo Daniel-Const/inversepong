642a68f44e80cbf41362246aee3dbda65ea82ffd
Daniel Constantinidis <dancon98@hotmail.com>
2023-12-26 17:15:31 +1100

    - update instructions

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -5,10 +5,31 @@ export const Menu = ({ onNewGame }: { onNewGame: () => void }) => {
                 <div className="menu">
                     <h2>How to play</h2>
                     <h3>Player 1</h3>
-                    <p>Up: w Down: s</p>
+                    <p>
+                        Up: <b>W</b> Down: <b>S</b>
+                    </p>
                     <h3>Player 2</h3>
-                    <p>Up: o Down: l</p>
+                    <p>
+                        Up: <b>O</b> Down: <b>L</b>
+                    </p>
+                    <p>
+                        Press <b>G</b> to start the round
+                    </p>
                     <button onClick={onNewGame}>New Game</button>
+                    <p
+                        style={{
+                            paddingTop: "2em",
+                            paddingBottom: 0,
+                            margin: 0,
+                        }}
+                    >
+                        <a
+                            href="https://github.com/Daniel-Const/inversepong"
+                            target="_blank"
+                        >
+                            Github
+                        </a>
+                    </p>
                 </div>
             </div>
         </>
